15804a0cbe557e323add9d6ac042a185bd939d92
Fred <castelloiv@gmail.com>
2021-08-24 08:51:52 -0400

    Added more webpack/eslint config

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => <h1>My React and TypeScript App!</h1>;
-
+const App = () => {
+  const unused = "something";
+  return <h1>My React and TypeScript App!</h1>;
+};
 ReactDOM.render(
   <React.StrictMode>
     <App />
