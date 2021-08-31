41fbb6ca675a73621b57049e3d47ad79eca29062
Fred <castelloiv@gmail.com>
2021-08-31 16:55:04 -0400

    Tailwind UI conversion from React to TS

--- src/configs/routing.tsx
+++ src/configs/routing.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Hero from "../features/dashboard/dashboard";
+import Hero from "../features/dashboard/dashboard2";
 
 const routes = (
   <Switch>
